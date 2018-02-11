81301e3ccc34a9477462b6173e70ef37e1799072
David Todd (c0de) <c0de@gama.io>
2018-02-10 21:36:07 -0600

    Automatic updates
    By default, this should prompt to update once every 14 days.
    If user answers anything other than y/Y, wait until next cycle
    If user answers y/Y, pull down the current branch's dotfiles
    (ensure that your machine is on the correct branch if you have multiple)

---
+++ upgrade.sh
@@ -0,0 +1,37 @@
+
+# Use colors, but only if connected to a terminal, and that terminal
+# supports them.
+if which tput >/dev/null 2>&1; then
+    ncolors=$(tput colors)
+fi
+if [ -t 1 ] && [ -n "$ncolors" ] && [ "$ncolors" -ge 8 ]; then
+  RED="$(tput setaf 1)"
+  GREEN="$(tput setaf 2)"
+  YELLOW="$(tput setaf 3)"
+  BLUE="$(tput setaf 4)"
+  BOLD="$(tput bold)"
+  NORMAL="$(tput sgr0)"
+else
+  RED=""
+  GREEN=""
+  YELLOW=""
+  BLUE=""
+  BOLD=""
+  NORMAL=""
+fi
+
+printf "${BLUE}%s${NORMAL}\n" "Updating your dotfiles"
+cd "$DOTFILES"
+branch=$(git name-rev --name-only HEAD)
+if git pull --rebase --stat origin $branch
+then
+  printf '%s' "$GREEN"
+  printf '%s\n' ' _____     ______     ______   ______   __     __         ______     ______    '
+  printf '%s\n' '/\  __-.  /\  __ \   /\__  _\ /\  ___\ /\ \   /\ \       /\  ___\   /\  ___\   '
+  printf '%s\n' '\ \ \/\ \ \ \ \/\ \  \/_/\ \/ \ \  __\ \ \ \  \ \ \____  \ \  __\   \ \___  \  '
+  printf '%s\n' ' \ \____-  \ \_____\    \ \_\  \ \_\    \ \_\  \ \_____\  \ \_____\  \/\_____\ '
+  printf '%s\n' '  \/____/   \/_____/     \/_/   \/_/     \/_/   \/_____/   \/_____/   \/_____/ '
+  printf "${BLUE}%s\n" "Hooray! Your dotfiles have been updated and/or is at the current version."
+else
+  printf "${RED}%s${NORMAL}\n" 'There was an error updating. Try again later?'
+fi
