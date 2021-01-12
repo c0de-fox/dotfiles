73598f29c9252e84bd47cb1e731e4250af4ea58a
David Todd <dtodd@oceantech.com>
2021-01-12 13:56:08 -0600

    improve generate pacman.lst script

--- generate_pacman.lst.sh
+++ generate_pacman.lst.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
-# Exports all installed packages, including foreign ones (AUR)
+# Exports all installed packages to pacman.lst. One for official
+# packages, and another for any packages that come from AUR.
+
 # This can be used as a reference, or to reinstall packages with:
-# `cat pacman.lst | xargs pacman -S --needed --noconfirm`
+# `cat pacman.lst | xargs pacman -S --needed --noconfirm` and
+# `cat pacman_aur.lst | xargs yay -S --needed --noconfirm`
+# (assuming yay is your AUR helper)
 
 pacman -Qqe | grep -v "$(pacman -Qqm)" > pacman.lst
+pacman -Qqm > pacman_aur.lst
