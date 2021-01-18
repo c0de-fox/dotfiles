f107d11d9b4b38a002b461e46c427c6ca89e1ff7
c0de <c0de@c0de.dev>
2021-01-17 20:03:39 -0600

    improve install of arch packages

--- generate_pacman.lst.sh
+++ generate_pacman.lst.sh
@@ -3,8 +3,8 @@
 # packages, and another for any packages that come from AUR.
 
 # This can be used as a reference, or to reinstall packages with:
-# `cat pacman.lst | xargs pacman -S --needed --noconfirm` and
-# `cat pacman_aur.lst | xargs yay -S --needed --noconfirm`
+# `pacman -S --needed --noconfirm $(cat pacman.lst) and`
+# `yay -S --needed --noconfirm $(cat pacman_aur.lst)`
 # (assuming yay is your AUR helper)
 
 pacman -Qqe | grep -v "$(pacman -Qqm)" > pacman.lst
